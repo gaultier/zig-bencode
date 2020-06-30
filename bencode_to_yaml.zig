d8a7e07d6b8478b33ca08a48155c2dccff367384
Philippe Gaultier <philippe.gaultier@ppro.com>
2020-06-30 17:24:52 +0200

    Output strings as utf8 if fitting, otherwise hexadecimal

--- bencode_to_yaml.zig
+++ bencode_to_yaml.zig
@@ -1,28 +1,93 @@
 const std = @import("std");
 const bencode = @import("src/main.zig");
 
+fn outputUnicodeEscape(
+    codepoint: u21,
+    out_stream: var,
+) !void {
+    if (codepoint <= 0xFFFF) {
+        // If the character is in the Basic Multilingual Plane (U+0000 through U+FFFF),
+        // then it may be represented as a six-character sequence: a reverse solidus, followed
+        // by the lowercase letter u, followed by four hexadecimal digits that encode the character's code point.
+        try out_stream.writeAll("\\u");
+        try std.fmt.formatIntValue(codepoint, "x", std.fmt.FormatOptions{ .width = 4, .fill = '0' }, out_stream);
+    } else {
+        std.debug.assert(codepoint <= 0x10FFFF);
+        // To escape an extended character that is not in the Basic Multilingual Plane,
+        // the character is represented as a 12-character sequence, encoding the UTF-16 surrogate pair.
+        const high = @intCast(u16, (codepoint - 0x10000) >> 10) + 0xD800;
+        const low = @intCast(u16, codepoint & 0x3FF) + 0xDC00;
+        try out_stream.writeAll("\\u");
+        try std.fmt.formatIntValue(high, "x", std.fmt.FormatOptions{ .width = 4, .fill = '0' }, out_stream);
+        try out_stream.writeAll("\\u");
+        try std.fmt.formatIntValue(low, "x", std.fmt.FormatOptions{ .width = 4, .fill = '0' }, out_stream);
+    }
+}
+
 fn dump(value: bencode.Value, indent: usize) anyerror!void {
+    var out_stream = std.io.getStdOut().writer();
+
     switch (value) {
         .Integer => |n| {
-            try std.io.getStdOut().writer().print("{}", .{n});
+            try out_stream.print("{}", .{n});
         },
         .String => |s| {
-            try std.io.getStdOut().writer().print("\"{}\"", .{s});
+            var i: usize = 0;
+
+            try out_stream.print("\"", .{});
+            if (std.unicode.utf8ValidateSlice(s)) {
+                while (i < s.len) : (i += 1) {
+                    switch (s[i]) {
+                        // normal ascii character
+                        0x20...0x21, 0x23...0x2E, 0x30...0x5B, 0x5D...0x7F => |c| try out_stream.writeByte(c),
+                        // only 2 characters that *must* be escaped
+                        '\\' => try out_stream.writeAll("\\\\"),
+                        '\"' => try out_stream.writeAll("\\\""),
+                        // solidus is optional to escape
+                        '/' => {
+                            try out_stream.writeByte('/');
+                        },
+                        // control characters with short escapes
+                        // TODO: option to switch between unicode and 'short' forms?
+                        0x8 => try out_stream.writeAll("\\b"),
+                        0xC => try out_stream.writeAll("\\f"),
+                        '\n' => try out_stream.writeAll("\\n"),
+                        '\r' => try out_stream.writeAll("\\r"),
+                        '\t' => try out_stream.writeAll("\\t"),
+                        else => {
+                            const ulen = std.unicode.utf8ByteSequenceLength(s[i]) catch unreachable;
+                            // control characters (only things left with 1 byte length) should always be printed as unicode escapes
+                            if (ulen == 1) {
+                                const codepoint = std.unicode.utf8Decode(s[i .. i + ulen]) catch unreachable;
+                                try outputUnicodeEscape(codepoint, out_stream);
+                            } else {
+                                try out_stream.writeAll(s[i .. i + ulen]);
+                            }
+                            i += ulen - 1;
+                        },
+                    }
+                }
+            } else {
+                for (s) |c| {
+                    try out_stream.print("\\x{X}", .{c});
+                }
+            }
+            try out_stream.print("\"", .{});
         },
         .Array => |arr| {
             for (arr.items) |v| {
-                try std.io.getStdOut().writer().print("\n", .{});
-                try std.io.getStdOut().writer().writeByteNTimes(' ', indent);
-                try std.io.getStdOut().writer().print("- ", .{});
+                try out_stream.print("\n", .{});
+                try out_stream.writeByteNTimes(' ', indent);
+                try out_stream.print("- ", .{});
                 try dump(v, indent + 2);
             }
         },
         .Object => |obj| {
             var it = obj.iterator();
             while (it.next()) |kv| {
-                try std.io.getStdOut().writer().print("\n", .{});
-                try std.io.getStdOut().writer().writeByteNTimes(' ', indent);
-                try std.io.getStdOut().writer().print("\"{}\": ", .{kv.key});
+                try out_stream.print("\n", .{});
+                try out_stream.writeByteNTimes(' ', indent);
+                try out_stream.print("\"{}\": ", .{kv.key});
                 try dump(kv.value, indent + 2);
             }
         },
